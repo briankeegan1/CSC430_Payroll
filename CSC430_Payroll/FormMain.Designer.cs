e8d76e6b4018a1866663dc302405f455b0b0a2ee
briankeegan1 <briankeeganmusic@gmail.com>
2018-09-13 18:50:07 -0400

    Added functioning Delete Employee button with confirmation.

--- CSC430_Payroll/FormMain.Designer.cs
+++ CSC430_Payroll/FormMain.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -81,11 +82,22 @@
             this.label1.Text = "Employees";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 448);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Delete Employee";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(841, 470);
+            this.ClientSize = new System.Drawing.Size(847, 479);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
@@ -104,5 +116,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- CSC430_Payroll/FormMain.cs
+++ CSC430_Payroll/FormMain.cs
@@ -50,5 +50,42 @@ namespace CSC430_Payroll
         {
             
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure to delete this Employee's records?",
+                "Confirm Deletetion", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                    string connectionString = ConfigurationManager.ConnectionStrings["db"].ConnectionString; //loading connection string from App.config
+                    SqlConnection con = new SqlConnection(connectionString); // making connection 
+                    con.Open();
+                    int rowID = int.Parse(dataGridView1[0, selectedIndex].Value.ToString());
+                    string sqlquery = "DELETE FROM Employee WHERE ID = " + rowID;
+
+                    try
+                    {
+                        SqlCommand command = new SqlCommand(sqlquery, con);
+                        command.ExecuteNonQuery();
+                        string cmdString = "SELECT ID, [Last Name], [First Name] FROM Employee";
+                        SqlDataAdapter sda = new SqlDataAdapter(cmdString, con);
+                        DataSet ds = new DataSet();
+                        sda.Fill(ds);
+                        dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                //do nothing
+            }
+        }
     }
 }
